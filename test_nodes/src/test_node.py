e9b4b141850ccd5e8abe30c8c7d17b2009b383d0
Johvonna <johvonnamb_@hotmail.com>
2019-03-12 16:47:09 -0400

    Test_node for creatin bogus data used for testing driving_controller

---
+++ test_nodes/src/test_node.py
@@ -0,0 +1,55 @@
+import numpy as np
+import math as m
+import rospy
+import tf
+from controls.msg import TwoPointVector
+from geometry_msgs.msg import Quaternion, PoseStamped, Vector3, Point
+from powertrain.msg import PowertrainParams
+    
+# Trajectory straight line on y-axis
+traj_start = Point()
+traj_start.x = 0
+traj_start.y = 0
+
+traj_end = Point()
+traj_end.x = 0
+traj_end.y = 1000
+
+# Position
+ps = PoseStamped()
+ps.pose.position.x = 50
+ps.pose.position.y = 50
+ps.pose.position.z = 0
+
+# Orientation
+quaternion = tf.transformations.quaternion_from_euler(0, 0, 0)
+ps.pose.orientation.x = quaternion[0]
+ps.pose.orientation.y = quaternion[1]
+ps.pose.orientation.z = quaternion[2]
+ps.pose.orientation.w = quaternion[3]
+
+
+def shutdown_hook():
+    """ Called when ROS is exit """
+
+    rospy.loginfo("Shutting down localization node...")
+
+
+def main():
+    rospy.init_node("test_nodes")
+    rospy.loginfo("Starting Test Nodes")
+    rospy.on_shutdown(shutdown_hook)
+
+    #Subscribe to Pose node for position and orientation data
+    pose_pub = rospy.Publisher('localization/pose', PoseStamped, queue_size=10)
+
+    #Subscribe to trajectory node for path to follow
+    traj_pub = rospy.Publisher('path_planning/trajectory', TwoPointVector, queue_size=10)
+
+    while not rospy.is_shutdown():
+        traj_pub.publish(TwoPointVector(traj_start, traj_end))
+        pose_pub.publish(ps)
+
+
+if __name__ == "__main__":
+    main()
